7f470e24a64f92580a6a433a47c628634112b8a3
Kamesh Kumar <kameshkumar4602@gmail.com>
2024-11-11 01:28:39 +0530

    Formatting

--- src/main/java/com/project/blog/exceptions/ResourceAlreadyPresentException.java
+++ src/main/java/com/project/blog/exceptions/ResourceAlreadyPresentException.java
@@ -6,9 +6,11 @@ import lombok.Setter;
 @Getter
 @Setter
 public class ResourceAlreadyPresentException extends RuntimeException{
+	
 	String resourceName;
 	String fieldName;
 	String fieldValue;
+	
 	public ResourceAlreadyPresentException(String resourceName, String fieldName, String fieldValue) {
 		super(String.format("%s is already present with %s : %s", resourceName,fieldName,fieldValue));
 		this.resourceName = resourceName;
--- src/main/java/com/project/blog/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/project/blog/exceptions/ResourceNotFoundException.java
@@ -6,9 +6,11 @@ import lombok.Setter;
 @Getter
 @Setter
 public class ResourceNotFoundException extends RuntimeException{
+	
 	String resourceName;
 	String fieldName;
 	Integer fieldValue;
+	
 	public ResourceNotFoundException(String resourceName, String fieldName, Integer fieldValue) {
 		super(String.format("%s not found with %s : %d", resourceName,fieldName,fieldValue));
 		this.resourceName = resourceName;
