86763a7b16fd4891c1df45c0e228eca8cec0d68a
Kamesh Kumar <kameshkumar4602@gmail.com>
2024-11-14 22:41:09 +0530

    Category Controller

---
+++ src/main/java/com/project/blog/controllers/CategoryController.java
@@ -0,0 +1,12 @@
+package com.project.blog.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/categories")
+public class CategoryController {
+	
+
+	
+}
